12482862ca3d2781009fb63ff23372887f49f140
James Carlson <jxxcarlson@gmail.com>
2021-05-04 21:40:21 -0400

    Complete the basic eval function, e.g, so as to evaluate (* 2 (+ 3 2))

--- README.md
+++ README.md
@@ -4,15 +4,19 @@ A draft implementation of a small version of Scheme following
 Jonathan Tang's article 
 [Write Yourself a Scheme in 48 Hours](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours).
 
-4/4/2021: Start project and write the parser
+4/4/2021: Start project, write the parser and a primitive `eval` function that can
+do basic integer arithmetic.
 
 
 ```lisp
 > run expr "(* 5 (+ 2 3) (u . 'v))"
-Ok (List [
-    Atom "*"
-  , Integer 5
-  , List [Atom "+", Integer 2, Integer 3]
-  , DottedList [Atom "u"] (List [Atom "quote", Atom "v"])])
+    Ok (List [
+      Atom "*"
+    , Integer 5
+    , List [Atom "+", Integer 2, Integer 3]
+    , DottedList [Atom "u"] (List [Atom "quote", Atom "v"])])
+  
+> run expr "(* 2 (+ 3 2))" |> Result.map eval
+    Ok (Integer 10)
 
 ```
--- src/Eval.elm
+++ src/Eval.elm
@@ -2,31 +2,64 @@ module Eval exposing (..)
 
 import  SchemeParser exposing(LispVal(..))
 import Dict exposing(Dict)
+import List.Extra
 
 
 {-|
+    > run expr "(+ 1 2)" |> Result.map eval
+    Ok (Integer 3)
+
+    > run expr "(* 2 (+ 3 2))" |> Result.map eval
+    Ok (Integer 10)
+
 
 -}
 eval : LispVal -> LispVal
 eval val =
     case val of
-        List items -> List (List.map eval items)
         Atom s -> Atom s
-        -- DottedList (List items) v -> DottedList (List (List.map eval items)) (eval v)
         DottedList stuff v -> DottedList (List.map eval stuff) (eval v)
         Integer n -> Integer n
         String s -> String s
         Bool s -> Bool s
+        (List (Atom func :: args)) -> apply func (List.map eval args)
+        List items -> List (List.map eval items)
 
-
+apply : String -> (List LispVal) -> LispVal
+apply func args =
+    case Dict.get func primitives of
+        Just f -> f args
+        Nothing -> Atom "No such function"
 
 
 primitives :  Dict String (List LispVal -> LispVal)
-primitives = [("+", numericBinop (+)),
-              --("-", numericBinop (-)),
-              --("*", numericBinop (*)),
-              --("/", numericBinop div),
+primitives = Dict.fromList [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop (//))
               --("mod", numericBinop mod),
               --("quotient", numericBinop quot),
               --("remainder", numericBinop rem)
               ]
+
+numericBinop : (Int -> Int -> Int) -> List LispVal -> LispVal
+numericBinop op params =
+    Integer <| op (getNum 0 params)  (getNum 1 params)
+
+getNum : Int -> List LispVal -> Int
+getNum k list =
+    List.Extra.getAt k list |> Maybe.map unpackNum |> Maybe.withDefault 0
+
+unpackNum : LispVal -> Int
+unpackNum val =
+    case val of
+        Integer n -> n
+        String n -> String.toInt n |> Maybe.withDefault 0
+        List data -> List.head data |> Maybe.map unpackNum |> Maybe.withDefault 0
+        _ -> 0
+
+
+
+
+-- Number $ foldl1 op $ map unpackNum params
+
